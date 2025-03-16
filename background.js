6df9b23e4c28b02d5090aee3f5b293db1802b696
ankursasmal <ankursasmal2017@gmail.com>
2025-03-16 11:02:32 +0530

    complete

--- background.js
+++ background.js
@@ -1,9 +1,24 @@
+function pauseVideo() {
+    const video = document.querySelector("video");
+    if (video) {
+        video.pause();
+    }
+}
+
+ function playVideo() {
+    const video = document.querySelector("video");
+    if (video) {
+        video.play();
+    }
+}
+
+
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
-    const tab = await chrome.tabs.get(activeInfo.tabId);
+         const tab = await chrome.tabs.get(activeInfo.tabId);
     if (tab.url && tab.url.includes("youtube.com/watch")) {
-        chrome.scripting.executeScript({
+           chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: playVideo
+               func: playVideo
         });
     }
 });
@@ -24,18 +39,4 @@ chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
     });
 });
 
-// Function to pause video
-function pauseVideo() {
-    const video = document.querySelector("video");
-    if (video) {
-        video.pause();
-    }
-}
-
-// Function to play video
-function playVideo() {
-    const video = document.querySelector("video");
-    if (video) {
-        video.play();
-    }
-}
+ 
